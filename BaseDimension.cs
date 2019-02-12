5b9cbb80be27e24791e88dd60be4426ebabef764
AlexC84 <25059737+AlexC84@users.noreply.github.com>
2019-02-12 22:22:06 +0000

    add additional class files to tracking

---
+++ BaseDimension.cs
@@ -0,0 +1,10 @@
+namespace hello_world
+{
+    public class BaseDimension
+    {
+        public string Symbol { get; private set; }
+        public string Name { get; private set; }
+        public BaseUnit BaseUnit { get; private set; }
+
+    }
+}
---
+++ BaseUnit.cs
@@ -0,0 +1,8 @@
+namespace hello_world
+{
+    public class BaseUnit
+    {
+        public string Symbol { get; private set; }
+        public string Name { get; private set; }
+    }
+}
