dc855c70058e00b4d16229a39335e2548c6a10d5
AlexC84 <25059737+AlexC84@users.noreply.github.com>
2019-02-12 22:20:43 +0000

    split class defn files into seperate files

--- SystemOfMeasurement.cs
+++ SystemOfMeasurement.cs
@@ -4,24 +4,12 @@ namespace hello_world
 {
     public class SystemOfMeasurement
     {
-        public SystemOfMeasurement(){
+        public SystemOfMeasurement()
+        {
             /* initiate system with;
              base dimensions
              base units
             */
         }
-    }
-
-    public class BaseDimension{
-        public string Symbol { get; private set; }
-        public string Name { get; private set; }
-        public BaseUnit BaseUnit { get; private set; }
-
-    }
-
-    public class BaseUnit
-    {
-        public string Symbol { get; private set; }
-        public string Name { get; private set; }
     }
 }
